00f67ac97e8f452028e316294eca11bd84705944
c-sp <66515552+c-sp@users.noreply.github.com>
2021-08-02 17:48:12 +0000

    Deploying to gh-pages from @ c-sp/age-online@a23c324b8f574c49ad24a825519c3ed491a27d38 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cf40d703f230a086b386.js"
+    "url": "webpack-runtime-0ea428bf5748fd72d08f.js"
   },
   {
     "url": "framework-b184e9a11f9b35f26f7b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6ae255074de6eb904696d0eb204f8dd6"
+    "revision": "cb30aee926deff3b635688fe62867235"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ce4cee30ef32ea09476a.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c6b8efe2473a8832b0f3670d64f348bc"
+    "revision": "ff4d1cf418ce5b350726c7d9fc080eb9"
   },
   {
     "url": "polyfill-bd6ac05d9a7014d4831d.js"
